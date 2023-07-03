73f3cb798915c5e0847b8a188754dba99a762bce
meruga-neha <136068488+meruga-neha@users.noreply.github.com>
2023-07-03 06:47:10 -0700

    Add files via upload

--- assignment4.py
+++ assignment4.py
@@ -1,5 +1,5 @@
 def my_function(num):
     return num + num + num
-number = list(map(int,input().split()))
+number = list(map(int,input('enter the list of numbers:').split()))
 triple_number = map(my_function,number)
-print(list(triple_number))
+print('list of triple numbers:',(list(triple_number)))
