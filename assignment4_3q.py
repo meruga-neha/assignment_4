2c4b0f0b9fe1ac53c4d503d231b12fa0f6da62c4
meruga-neha <136068488+meruga-neha@users.noreply.github.com>
2023-07-03 06:50:44 -0700

    Add files via upload

--- assignment4_3q.py
+++ assignment4_3q.py
@@ -1,5 +1,5 @@
 def square_function(n):
     return n * n
-number = list(map(int,input().split()))
+number = list(map(int,input('enter the list of numbers:').split()))
 square_number = map(square_function,number)
-print(list(square_number))
+print('list of square numbers:',(list(square_number)))
